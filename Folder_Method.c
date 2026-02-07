a78c92dfe4bf72662d73df6d7b80663845dedf38
RadeonRx7600 <187996749+RadeonRx7600@users.noreply.github.com>
2026-02-07 01:08:54 +0100

    First Operational Version
    v 1.0

--- Folder_Method.c
+++ Folder_Method.c
@@ -1,12 +1,26 @@
 #include <dirent.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <openssl/sha.h>
 #include <sys/stat.h>
 
+//Color System >>
+#define GREEN "\x1b[32m"
+#define YELLOW "\x1b[33m"
+#define RED "\x1b[31m"
+#define RESET "\x1b[0m"
+
+//  gcc Folder_Method.c -o Folder_Method -lcrypto
+
+//----------------------------------------------------------------------------------------------------------------
+
 void SHA256test(const char *dir_file1 , const char *dir_file2)
 {
+
+    //debugging path ;D u can delete it if u want, or need to creat a feature about it.
+    printf("debbuging folder1 sha256 = %s\n",dir_file1);
+    printf("debbuging folder2 sha256 = %s\n",dir_file2);
+    
     //FUNCTION ___STATUS(OK)___
     
     unsigned char hash1[SHA256_DIGEST_LENGTH];
@@ -45,25 +59,43 @@ void SHA256test(const char *dir_file1 , const char *dir_file2)
 
 
     if (memcmp(hash1, hash2, SHA256_DIGEST_LENGTH) == 0) {
-        printf(stdout,"\x1b[32mHASH : same\x1bn[0m\n");//green
+        fprintf(stdout, GREEN "HASH : identical" RESET "\n"); //green
+        printf("\n");
     } 
     else {
-        printf(stdout,"\x1b[31mHASH : not the same\x1bn[0m\n");//red
+        fprintf(stdout, RED "HASH : wrong(not the same !)" RESET "\n"); //red
+        printf("\n");
     }
 }
 
 //----------------------------------------------------------------------------------------------------------------
 
-int check_file(const char *file, const char *folder2) {
+int check_file(const char *file,const char *folder1, const char *folder2) {
+
+    //FUNCTION __STATUS(OK)__
+    
     DIR *dirf2 = opendir(folder2);
-    if (!dirf2) return 0;
+    if (!dirf2) {
+        return 1;
+    }
 
     struct dirent *entry2;
+    
     while ((entry2 = readdir(dirf2)) != NULL) {
         if (strcmp(entry2->d_name, ".") == 0 || strcmp(entry2->d_name, "..") == 0)
             continue;
+        
         if (strcmp(file, entry2->d_name) == 0) {
-            printf("same file is %s\n", file);
+            printf("%sOK : same file >> %s%s\n", GREEN, file, RESET);
+
+            char file_path_folder1[1024];
+            char file_path_folder2[1024];
+
+            snprintf(file_path_folder1, sizeof(file_path_folder1), "%s/%s", folder1, file);
+            snprintf(file_path_folder2, sizeof(file_path_folder2), "%s/%s", folder2, file);
+
+            SHA256test(file_path_folder1, file_path_folder2);
+            
             closedir(dirf2);
             return 1;
         }
@@ -72,9 +104,12 @@ int check_file(const char *file, const char *folder2) {
     return 0;
 }
 
-void Pathfinder(const char *folder1 , const char *folder2)
-//FOLDER_FINDING __STATUS__(NOTOK)
-{
+//----------------------------------------------------------------------------------------------------------------
+
+void Pathfinder(const char *folder1 , const char *folder2){
+
+    //FOLDER_FINDING __STATUS__(NOTOK)
+
     DIR *dirf1 = opendir(folder1);
     if (!dirf1) 
     {
@@ -105,17 +140,18 @@ void Pathfinder(const char *folder1 , const char *folder2)
 
         if (stat(file_path_folder1, &st1) == -1)
             continue;
+        
         if (stat(file_path_folder2, &st2) == -1)
             continue;
         
-
         if (S_ISDIR(st1.st_mode)) {
-            // si c’est un dossier → récursion
+            //directory (folder)
             Pathfinder(file_path_folder1,file_path_folder2);
         }
+            
         else if (S_ISREG(st1.st_mode)) {
-            // si c’est un fichier → affichage
-            check_file(entry1->d_name,folder2);
+            // register(file)
+            check_file(entry1->d_name,folder1 ,folder2);
         }
     }
 
@@ -123,12 +159,4 @@ void Pathfinder(const char *folder1 , const char *folder2)
     closedir(dirf2);
 }
 
-//-----------------------------------------------------------------
-
-int main() {
-    char *folder1 = "/home/juan/Bureau/BH_FolderMethod_test/test_orignal";
-    char *folder2 = "/home/juan/Bureau/BH_FolderMethod_test/test_modified";
-
-    Pathfinder(folder1, folder2);
-    return 0;
-}
+// version 1.0.0 (no bug);
