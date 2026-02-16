b08bd3f5405411c98ea556c9851cc593cbc56954
RadeonRx7600 <187996749+RadeonRx7600@users.noreply.github.com>
2026-02-16 01:10:25 +0100

    Create BinaryHunt.c with main function
    Initial implementation of BinaryHunt tool with basic structure.

---
+++ BinaryHunt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// BinaryHunt is a tool to analyse modified file to their original;
+// It compares binary and turn it into a ASM code or binary;
+// started at : 3pm; 28/01/2026;
+// Github >> https://github.com/RadeonRx7600
+// MIT LICENSE >> https://opensource.org/license/mit ...soon
+
+int main() {
+
+  
+  return 0;
+  
+}
